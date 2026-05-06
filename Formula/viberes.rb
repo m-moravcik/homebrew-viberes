6404996ae8b2246865e98f43cc982c15d346ae39
Michal Moravčík <michal.moravcik@me.com>
2026-05-06 13:48:26 +0200

    viberes 0.3.0

--- Formula/viberes.rb
+++ Formula/viberes.rb
@@ -10,8 +10,8 @@
 class Viberes < Formula
   desc "Command-line companion for VibeRes (macOS resolution switcher)"
   homepage "https://github.com/m-moravcik/VibeRes"
-  url "https://github.com/m-moravcik/VibeRes.git", tag: "v0.1.0", revision: nil
-  version "0.1.0"
+  url "https://github.com/m-moravcik/VibeRes.git", tag: "v0.3.0", revision: nil
+  version "0.3.0"
   license "MIT"
   head "https://github.com/m-moravcik/VibeRes.git", branch: "main"
 
